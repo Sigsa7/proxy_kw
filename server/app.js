d1ec41c464870b552b903a4cb31fad7ac871f2b4
Katherine Wang <kwang004@gmail.com>
2019-08-08 12:02:32 -0700

    Add endpoints for reviews and images

--- server/app.js
+++ server/app.js
@@ -7,11 +7,13 @@ const apiProxy = httpProxy.createProxyServer();
 const app = express();
 app.use(express.static(path.join(__dirname, '/../public/')));
 app.use('/:restaurantID', express.static(path.join(__dirname, '/../public/')));
-const serverReservations = 'http://localhost:3005'
+const serverReservations = 'http://18.217.25.48';
+const reviews = 'http://54.153.40.76';
+const photos = 'http://13.52.61.135';
 
 
 app.all('/booking/reserved/:restaurantID', function(req, res) {
-    apiProxy.web(req, res, {target: serverReservations});
+  apiProxy.web(req, res, {target: serverReservations});
 });
 
 app.all('/booking/count/:restaurantID', function(req, res) {
@@ -29,5 +31,19 @@ app.all('/booking/update/:restaurantID', function(req, res) {
 app.all('/booking/cancel/:restaurantID', function(req, res) {
   apiProxy.web(req, res, {target: serverReservations});
 });
+
+app.all("/:restaurantID/reviews/reviewListing", function(req, res) {
+  console.log('redirecting to reviews api');
+  apiProxy.web(req, res, {target: reviews});
+});
+app.all("/:restaurantID/reviews/restaurantInfo", function(req, res) {
+  console.log('redirecting to reviews api');
+  apiProxy.web(req, res, {target: reviews});
+});
+
+app.all('/:restaurantID/images', function(req, res) {
+  apiProxy.web(req, res, {target: photos});
+});
+
 
 module.exports = app;
